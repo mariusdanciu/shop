57a0ecf2c08473bd23ec3f56efef0e832c0bb9c2
Marius Danciu <marius.danciu@gmail.com>
2014-04-04 15:06:03 +0300

    updated

--- shopweb/web/scripts/products.js
+++ shopweb/web/scripts/products.js
@@ -2,33 +2,53 @@
 	$(function() {
 		$(".item_box").each(function(index) {
 			var me = $(this);
-
-			$(this).mouseenter(function() {
+			
+			me.mouseenter(function() {
+				me.css({
+					'cursor' : 'pointer'
+				});
 				me.find('.info_tag').css({
-					background : 'rgba(0, 0, 0, 1)'
+					'background' : 'rgba(0, 0, 0, 1)'
 				});
 				me.find('.info_tag div').css({
 					'color' : '#ffffff'
 				});
-				me.find('.info_tag_icons').css({
-					visibility : 'visible'
+				me.find('.info_tag_cart').css({
+					'display' : 'inline'
 				});
+				
 			});
 
-			$(this).mouseleave(function() {
+			me.mouseleave(function() {
+				me.css({
+					'cursor' : 'hand'
+				});
 				me.find('.info_tag').css({
 					'background' : 'rgba(255, 255, 255, .5)'
 				});
 				me.find('.info_tag div').css({
 					'color' : '#000000'
 				});
-				me.find('.info_tag_icons').css({
-					visibility : 'hidden'
+				me.find('.info_tag_cart').css({
+					'display' : 'none'
 				});
 			});
-
+			
+			me.find('.info_tag_cart').click(function(event) {
+				var pid = me.attr("id");
+				cart.addItem(pid);
+				cart.showCart();
+				event.stopPropagation();
+			});
+			
+			me.click(function(event) {
+				var pid = me.attr("id");
+				window.location.href = "/product?pid=" + pid;
+				event.stopPropagation();
+			});
+			
 		});
-
+		
 	});
 
 })();
