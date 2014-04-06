cbbd4e0eadbba81953dea0a8d7391cc87f397b2d
Marius Danciu <marius.danciu@gmail.com>
2014-04-06 13:14:07 +0300

    fix

--- shopweb/src/main/scala/net/shop/backend/impl/CachingBackend.scala
+++ shopweb/src/main/scala/net/shop/backend/impl/CachingBackend.scala
@@ -5,6 +5,7 @@ import net.shop.model.ProductDetail
 import scala.util.Try
 import net.shop.model.Category
 import scala.util.Success
+import scala.util.Failure
 
 class CachingBackend(serv: ProductsService) extends ProductsService {
 
@@ -22,12 +23,18 @@ class CachingBackend(serv: ProductsService) extends ProductsService {
   def filter(f: ProductDetail => Boolean): Try[Traversable[ProductDetail]] = allProducts match {
     case Success(prods) => Success(for (p <- prods if f(p)) yield p)
     case f => f
-  } 
-    
-  def categoryById(id: String): Try[Category] = (for {
-    p <- categories if (!p.find(c => c.id == id).isEmpty)
-  } yield p).map { l => l.head }
+  }
 
+  def categoryById(id: String): Try[Category] = {
+    categories match {
+      case Success(cats) =>
+        cats.find(_.id == id) match {
+          case Some(c) => Success(c)
+          case _ => Failure(new Exception("Category not found"))
+        }
+      case Failure(f) => Failure(f)
+    }
+  }
   def allCategories = categories
 
   def searchProducts(text: String): Try[Traversable[ProductDetail]] = serv.searchProducts(text)
