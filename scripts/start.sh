a1e5a9d4b7958a211605528612670d4122221182
marius <marius.danciu@gmail.com>
2015-04-05 23:55:29 +0300

    na

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 mkdir ./logs
 
-java -Xms256m -Xmx700m -classpath .:./lib/* net.shop.web.StartShop -Dlog4j.configuration=./config/log4j.properties 2>&1 | tee ./logs/server.out &
+java -Xms256m -Xmx700m -classpath .:./lib/* net.shop.web.StartShop -Dlog4j.configuration=./config/log4j.properties > ./logs/console.log  2>&1 &
 
 echo $! > ./server.pid
